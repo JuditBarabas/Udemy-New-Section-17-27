4c95c39f79a967a899b181688151b044dfd9ab19
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-25 11:36:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('For solution open js file');
+
+// prewritten
+const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter'];
+
+// YOUR CODE GOES BELOW THIS LINE:
+planets.shift();
+planets.push('Saturn');
+planets.unshift('Mercury');
